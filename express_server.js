5114e5c90cd852a16a7ef55ce68a07cbc405a67d
Nicola <va3nky@hotmail.com>
2018-09-04 21:05:49 +0000

    implemented an update form and an edit anchor tag that leads to the update page

--- express_server.js
+++ express_server.js
@@ -45,6 +45,11 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.long_URL
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
    let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
