743b04d258a39695064e8606355ffa3d215edb0a
Nicola <va3nky@hotmail.com>
2018-09-04 18:47:50 +0000

    implemented ejs templates and created a submission form that lets a user enter a url and adds a shorturl to the database

---
+++ express_server.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+   let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
+
+function generateRandomString() {
+  var id = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    id += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return id;
+}
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
---
+++ test.js
@@ -0,0 +1,11 @@
+function makeid() {
+  var id = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    id += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return id;
+}
+
+console.log(makeid());
