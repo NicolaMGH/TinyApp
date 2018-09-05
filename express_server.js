37afb5c8b2b0c59f4755d39f644ff6714ca1609e
Nicola <va3nky@hotmail.com>
2018-09-05 12:19:03 +0000

    implemented a login and logout if statement in _header.js and also implemented the server logic so that when a user hits logout it clears the cookies and doesnt show their username

--- express_server.js
+++ express_server.js
@@ -1,20 +1,22 @@
-var express = require("express");
-var app = express();
-var cookieParser = require('cookie-parser')
+const express = require("express");
+const app = express();
+const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-var PORT = 8080; // default port 8080
+const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
+  let templateVars = {username: req.cookies["username"]};
   res.send("Hello!");
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -22,16 +24,19 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
+  let templateVars = {username: req.cookies["username"]};
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -46,6 +51,11 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
 //deletes url
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
@@ -53,26 +63,28 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.long_URL
+  urlDatabase[req.params.id] = req.body.long_URL;
   res.redirect("/urls");
 });
 
 app.get("/u/:shortURL", (req, res) => {
-   let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  let templateVars = {username: req.cookies["username"]};
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL, templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
 function generateRandomString() {
-  var id = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let id = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 6; i++)
+  for (var i = 0; i < 6; i++) {
     id += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
 
   return id;
 }
